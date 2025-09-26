a0a95a8f704d04a189c7feaade60b3569583a31f
iansh1 <is27382p@pace.edu>
2025-09-25 20:30:19 -0400

    rework middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,10 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function middleware(_request: NextRequest) {
   return NextResponse.next();
 }
 
 export const config = {
   matcher: ['/((?!_next).*)'],
-  runtime: 'nodejs',
 };
